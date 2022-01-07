e913ff923ca375f2c705258b6604d2f5c1154a99
A <alphanow@gmail.com>
2022-01-07 23:28:05 +0800

    Modify pmlist to pmlists

--- index.js
+++ index.js
@@ -2,22 +2,19 @@ import { getPageSource, getPageBlocks } from './wiki.js';
 import pLimit from 'p-limit';
 import { writeFile, appendFile } from 'fs/promises';
 
-const limit = pLimit(5);
+const limit = pLimit(10);
 
 const getAllPmNames = async () => {
-    const templates = {
-        'rdexn': 3, 'rdexn/2': 4, 'Rdexn/K': 5
-    };
-    const headers = [...Object.keys(templates)];
-
-    const blocks = await getPageBlocks('宝可梦列表（按全国图鉴编号）');
-    const pms = blocks.filter((i) => headers.includes(i[0]));
-    const names = pms.map((i) => i[templates[i[0]]]);
+    const blocks = await getPageBlocks('宝可梦列表（按全国图鉴编号）/简单版');
+    const pms = blocks.filter((i) => i[0] === 'Rdexe');
+    const names = pms.map((i) => i[2]);
     return names;
 };
 const getPmInfo = async (name) => {
+    const blockTypes = ['寶可夢信息框', '寶可夢信息框/形態', '寶可夢信息框/形态'];
     const blocks = await getPageBlocks(name);
-    const infoBlock = blocks.find((i) => i[0] === '寶可夢信息框');
+    const infoBlock = blocks.find((i) => blockTypes.includes(i[0]));
+    if (!infoBlock) { console.log(name); return {} }
     const infoObj = infoBlock.slice(1).reduce((r, i) => {
         const [k, v] = i.split('=');
         r[k] = v;
@@ -37,6 +34,7 @@ const saveCSV = async (name, data) => {
 };
 const main = async () => {
     const names = await getAllPmNames();
+    writeFile('pmlist.txt', names.join('\n'));
     const pmInfo = await Promise.all(names.map((name) => limit(getPmInfo, name)));
 
     await saveCSV('pm', pmInfo);
